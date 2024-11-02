e15cccf5fceda321e6110e352c3ee54bd47f00de
Ramavath Elisha Naik <ramavathelishanaikrpsyken@gmail.com>
2024-11-02 11:21:04 +0530

    url updated again

--- src/quries.js
+++ src/quries.js
@@ -6,7 +6,7 @@ export const useGetImages = (queryImage) => {
     queryKey: ["images", queryImage],
     queryFn: async ({ queryKey }) => {
       const response = await axios.get(
-        `https://www.api.unsplash.com/search/photos?client_id=${
+        `https://api.unsplash.com/search/photos?client_id=${
           import.meta.env.VITE_API_KEY
         }&&query=${queryKey[1]}`
       );
