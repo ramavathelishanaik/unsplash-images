3205cbdc4ba47538be302b2d64f1501830eb83b3
Ramavath Elisha Naik <ramavathelishanaikrpsyken@gmail.com>
2024-11-02 11:16:14 +0530

    url updated

--- src/quries.js
+++ src/quries.js
@@ -1,17 +1,17 @@
-import {
-    useQuery,
-  } from "@tanstack/react-query";
-  import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-  export const useGetImages = (queryImage) => {
-    const { data, isLoading,isError } = useQuery({
-      queryKey: ["images", queryImage],
-      queryFn: async ({ queryKey }) => {
-        const response = await axios.get(
-            `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}&&query=${queryKey[1]}`
-        );
-        return response.data;
-      },
-    });
-    return { data, isLoading,isError };
-  };
+export const useGetImages = (queryImage) => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["images", queryImage],
+    queryFn: async ({ queryKey }) => {
+      const response = await axios.get(
+        `https://www.api.unsplash.com/search/photos?client_id=${
+          import.meta.env.VITE_API_KEY
+        }&&query=${queryKey[1]}`
+      );
+      return response.data;
+    },
+  });
+  return { data, isLoading, isError };
+};
